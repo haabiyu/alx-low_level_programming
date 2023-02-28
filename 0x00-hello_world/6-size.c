f9fd8091163d312f1b1af0e765814805a7ec5cbc
haabiyu <haabiyu17@gmail.com>
2023-02-28 01:27:07 -0800

    All 0x00.c - Hello, World tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * Your program should return 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a lont int: %d byte(s)", sizeof(long));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long));
+	printf("Size of a float: %d byte(s)", sizeof(float);
+	return(0);1
+} 
