4a9ef94415fc6f2178407abd898016fa7656a4ea
haabiyu <haabiyu17@gmail.com>
2023-03-15 23:45:35 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@ int main(void)
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 
-	for(i=0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		putchar(s[i]);
-
+	putchar('\0');
 	return (1);
 }
