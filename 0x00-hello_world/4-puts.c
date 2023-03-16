c115000d4fae729bdcc5abb8503a39179c942a75
haabiyu <haabiyu17@gmail.com>
2023-03-15 22:57:58 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-//*Write a string followed by a new line
+/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 0
+ */
 
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
