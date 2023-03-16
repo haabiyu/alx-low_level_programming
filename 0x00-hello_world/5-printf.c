b2f39faeeb22980726d1cb5d562d5e2a5773899a
haabiyu <haabiyu17@gmail.com>
2023-03-15 23:10:43 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-/*Print a sttring follwed by a new line
+/**
+ * main - Entry point
+ * Print a sttring follwed by a new line
  * Use the function printf
  * You are not allowed to use the function puts
- * Your program should return 0
  * Your program should compile without warning when using the -Wall gcc option
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
