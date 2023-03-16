87c86117bd9931be6f21bf5c029bf88dc74a94c4
haabiyu <haabiyu17@gmail.com>
2023-03-15 23:00:44 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- *
- * Use the function puts
+/*
+ * Write a C program that prints exactly 
+ * use the function puts
  * You are not allowed to use printf
  * Your program should end with the value 0
  */
