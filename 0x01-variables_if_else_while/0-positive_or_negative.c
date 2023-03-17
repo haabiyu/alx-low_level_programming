93165f18872bf943b97fccb4b85d0ec16727786c
haabiyu <haabiyu17@gmail.com>
2023-03-17 05:59:57 -0700

    Hot Fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-/** 
- * main - main block
+/**
+ * int main(void)
  *
  * assign a random number to the variable n each time it is execute
  * The number, followed by
@@ -11,7 +11,7 @@
  * if the number is 0: is zero
  * if the number is less than 0: is negative
  * followed by a new line
- * 
+ *
  * main - main block
  */
 
