261962ad165223fe3758accc48b6d7d50543746c
haabiyu <haabiyu17@gmail.com>
2023-03-17 06:33:50 -0700

    Hot Fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-/**Write a program that prints all the numbers of base 16 in lowercase, followed by a new line
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+/**
+ * main - main block
+ *
+ * numbers of base 16 in lowercase, followed by a new line
+ * putchar function (every other function (printf, puts, etc…) is forbidden)
  * All your code should be in the main function
  * You can only use putchar three times in your code
+ *
+ * Return: 0
  */
 
 int main(void)
 {
 	char a, b;
 
-	for(a='0'; a<='9'; a++)
+	for (a = '0'; a <= '9'; a++)
 		putchar(a);
-	for(b='a'; b<='f'; b++)
+	for (b = 'a'; b <= 'f'; b++)
 		putchar(b);
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
