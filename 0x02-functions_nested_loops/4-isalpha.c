cd97f0fbea8a3365b0c47875742259378c14fb71
haabiyu <haabiyu17@gmail.com>
2023-03-21 10:04:48 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-/**Write a function that checks for alphabetic character
- * Prototype: int _isalpha(int c);
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+/**
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
  */
-
-int _isalpha(int c);
+int _isalpha(int c)
 {
- if(c >= 65 && c <= 90 || c >= 97 && c <= 122)
-        return(1);
-    else
-        return(0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+
 }
