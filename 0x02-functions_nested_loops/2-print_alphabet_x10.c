26d7a2f5a2afceea6f73fd6e7e1566103c5f8a7c
haabiyu <haabiyu17@gmail.com>
2023-03-21 10:00:35 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char a;
 	int b;
 
-	for(b = 1; b <= 10; b++)
+	for (b = 1; b <= 10; b++)
 	{
-		for(a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
