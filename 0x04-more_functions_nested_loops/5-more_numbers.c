bcba048def8bcec5365576dc770dee2322258ea1
haabiyu <haabiyu17@gmail.com>
2023-03-23 10:56:53 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
-/**Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
- * Prototype: void more_numbers(void);
- * You can only use _putchar three times in your code
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
  */
 
-void more_numbers(void);
+void more_numbers(void)
 {
     int i;
 
-    for(i=0; i<=14; i++)
+    for (i = 0; i <= 14; i++)
     {
-        if(i>9)
+        if (i > 9)
         {
             putchar((i/10) + '0');
             putchar((i%10) + '0');
