e7e81fe13abf09f80d277a6d8e2cfb8d5eddcc7f
haabiyu <haabiyu17@gmail.com>
2023-03-27 10:39:53 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * puts_half - main function.
- * @str: Pointer, the string of numbers to print.
- *
- * Description: This function prints half of a string.
- *
- * Return: none.
- */
+* puts_half - main function.
+* @str: Pointer, the string of numbers to print.
+*
+* Description: This function prints half of a string.
+*
+* Return: none.
+*/
 
 void puts_half(char *str)
 {
-    int i;
-    int len_str = 0;
-    int half_str;
+	int i;
+	int len_str = 0;
+	int half_str;
 
-    for (i = 0; str[i] != '\0'; i++)     /*check the length of the string*/
-    {
-    	len_str++;
-    }
-    /*printf("%d", len_str);*/
+	for (i = 0; str[i] != '\0'; i++)     /*check the length of the string*/
+	{
+	len_str++;
+	}
+	/*printf("%d", len_str);*/
 
-    half_str = len_str / 2;         /*half the length of the string*/
-    /*printf("%d", half_str);*/
+	half_str = len_str / 2;         /*half the length of the string*/
+	/*printf("%d", half_str);*/
 
-    for (i = half_str; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	for (i = half_str; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
 }
