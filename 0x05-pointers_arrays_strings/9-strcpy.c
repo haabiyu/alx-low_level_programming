906fba1fd5af942904dff83852d5c6038792c095
haabiyu <haabiyu17@gmail.com>
2023-03-27 10:46:19 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
-/**Prototype: char *_strcpy(char *dest, char *src);
- * Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
- * Return value: the pointer to dest
- * FYI: The standard library provides a similar function: strcpy. Run man strcpy to learn more.
+/**
+ * *_strcpy - main function.
+ * @src: The source.
+ * @dest: The destination.
+ *
+ * Description: This function copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ *
+ * Return: The pointer to dest.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-    int i;
+	int i;
 
-    for (i = 0;src[i] != '\0'; i++)
-        dest[i] = src[i];
-    dest[i] = '\0';
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
