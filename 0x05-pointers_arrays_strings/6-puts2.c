39ba4e68a831e770ef50fdaf77ab2f28757648a1
haabiyu <haabiyu17@gmail.com>
2023-03-27 10:18:37 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@
 void puts2(char *str)
 {
 	int i;	/* declare integer variable i*/
-	
-	/* loop through the string, incrementing i by 2 each time to skip every other character*/
-	for (i = 0; str[i] != '\0'; i += 2) 
-	{ 
+
+	/* loop the string, incrementing i by 2 each time to skip character*/
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
 		_putchar(str[i]);	/* print the character at index i using _putchar*/
 	}
 	_putchar('\n');	/* print a newline character using _putchar*/
