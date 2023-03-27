67be5dff59c346fa1ab54b71e695d3abc66ee7ae
haabiyu <haabiyu17@gmail.com>
2023-03-27 07:03:55 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,27 @@
 #include "main.h"
 
-/**Write a function that prints a string, followed by a new line, to stdout.
- * Prototype: void _puts(char *str);
- * FYI: The standard library provides a similar function: puts. Run man puts to learn more.
+/**
+ * _puts - main function.
+ * @str: Pointer with the address value.
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: none.
  */
 
-void _puts(char *str);
+void _puts(char *str)
 {
-    int i;
+	int i;
 
-    while(str[i] != '\0')
-        {
-            _putchar(stri);
-            i++;
-        }
-        _putchar('\n');
-    return 0;
+	/* check if the character is not null,
+	 * * if true then go inside the loop*/
+	while (str[i] != '\0')
+	{
+		/* call our _putchar function instead of printf*/
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');	/* print new line after printing the string*/
+	
+	return 0;
 }
