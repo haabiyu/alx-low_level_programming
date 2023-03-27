fb89d86e4bb824e75105811be3dcaf3684b868bd
haabiyu <haabiyu17@gmail.com>
2023-03-27 06:42:39 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,20 +2,21 @@
 
  /**
   * _strlen - Returns the length of a string
-  * 
+  *
   * @s: Pointer to the string to be measured
-  * 
+  *
   * Return: Length of string
   */
 
 int _strlen(char *s)
 {
-    int count = 0;	/*Initialize variable to store length of string*/
+	int count = 0;		/*Initialize variable to store length of string*/
 
-    while (*s != '\0')/*Loop until the end of the string is reached*/
-    {
-        count++;	/*Increment the length by 1*/
-        s++;		/*Move the pointer to the next character*/
-    }
-    return (count);	/*Return the length of the string*/
+	while (*s != '\0')	/*Loop until the end of the string is reached*/
+	{
+		count++;	/*Increment the length by 1*/
+		s++;		/*Move the pointer to the next character*/
+	}
+
+	return (count);		/*Return the length of the string*/
 }
