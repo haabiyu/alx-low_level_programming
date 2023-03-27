197b836b46783ca02a8bb06f783d6545b97bcb33
haabiyu <haabiyu17@gmail.com>
2023-03-27 10:01:26 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include "string.h"
 
 /**
- * print_rev - main function.
+ * rev_string - main function.
  * @s: Pointer, the string to print.
  *
  * Description: This function prints a string in reverse.
@@ -15,12 +15,12 @@ void rev_string(char *s)
 	int i, j; /* declare integer variables i and j*/
 	char c; /* declare character variable c*/
 
-	/* loop through the string, swapping characters at opposite ends until the middle is reached*/
+	/* loop the string, swapping characters at opposite ends until the middle*/
 	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
 	{
 		/* store the character at index i in the temporary variable c*/
 		c = s[i];
 		s[i] = s[j];    /* set the character at index i to the character at index j*/
-		s[j] = c;       /* set the character at index j to the character stored in c*/
+		s[j] = c;       /* set character at index j to the character stored in c*/
 	}
 }
