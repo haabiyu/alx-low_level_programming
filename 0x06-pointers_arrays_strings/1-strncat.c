056ed07d0f872321364abe84067cb153033289e5
haabiyu <haabiyu17@gmail.com>
2023-03-30 05:37:38 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
-/**Write a function that concatenates two strings.
- * Prototype: char *_strncat(char *dest, char *src, int n);
- * The _strncat function is similar to the _strcat function, except that
- * it will use at most n bytes from src; and
- * src does not need to be null-terminated if it contains n or more bytes
- * Return a pointer to the resulting string dest
- * FYI: The standard library provides a similar function: strncat. Run man strncat to learn more.
- */
+/**
+*_strncat - Funtion
+*
+*@dest: Destiny
+*@src: Source
+*@n: Variable
+*Return: Destiny
+*/
+
 char *_strncat(char *dest, char *src, int n)
 {
-    int len_dest = 0, len_src, i;
+    int dest_len = 0;
+    int i;
 
-    for(len_dest=0; *(dest + len_dest) != '\0'; )
-        len_dest++;
-    //printf("\n%d\n\n", len_dest); <----- to check the string length of dest
-
-    for(len_src = 0; len_src < n; len_src++)
+    /* Find the length of the destination string*/
+    while (dest[dest_len] != '\0')
     {
-        *(dest + len_dest) = *(src + len_src);
-        if (*(src + len_src) == '\0')
-			break;
-		len_dest++;
+        dest_len++;
     }
-    return(dest);
+
+    /* Concatenate the strings*/
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[dest_len + i] = src[i];
+    }
+
+    /* Terminate the string*/
+    dest[dest_len + i] = '\0';
+
+    /* Return a pointer to the destination string*/
+    return dest;
 }
